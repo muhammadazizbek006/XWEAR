15ae9dfd4ab7b822baca7938aea90c5fd3888a16
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-11 17:42:00 +0300

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// img
+import logo from '../img/header/logo.svg'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='py-8 bg-black '>
+      <div className="containerb">
+      {/* logo */}
+      <Link to='/'>
+        <img src={logo} alt="logo" />
+      </Link>
+      </div>
+    </header>
   )
 }
 
