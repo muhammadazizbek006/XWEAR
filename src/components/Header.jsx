b2d5a79df3a7c15d2ff32392e3255cdec5c3b774
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-16 16:17:03 +0300

    add center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
+// data 
+import { links } from '../data/data'
 import { Link } from 'react-router-dom'
 // img
-import logo from '../img/header/logo.svg'
+import logo from '../img/logo.svg'
 const Header = () => {
   return (
     <header className='py-8 bg-black '>
-      <div className="containerb">
+      <div className="containerb flex justify-between ">
       {/* logo */}
       <Link to='/'>
         <img src={logo} alt="logo" />
       </Link>
+        {/* center */}
+        <ul className='flex space-x-12 items-center'>
+         {
+          links.map((e)=>{
+            return(
+              <li className='text-white text-sm font-semibold flex ' key={e.id}>
+                <Link to={e.URL}>{e.link}</Link>
+                {/* <img src={e.img} alt={e.link} /> */}
+              </li> 
+            )
+          })
+         }
+        </ul>
+        {/* right */}
+        
       </div>
     </header>
   )
---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+// header link
+import vector from '../img/vector.svg' 
+
+
+export const  links  = [
+    {
+        id:1,
+        link:`Одежда`,
+        URL:'/Одежда',
+        img:vector,
+
+    },
+    {
+        id:2,
+        link:`Обувь`,
+        URL:'/Обувь',
+        img:vector,
+
+    },
+    {
+        id:3,
+        link:`Аксессуары`,
+        URL:'/Аксессуары',
+        img:vector,
+
+    },
+    {
+        id:4,
+        link:`Бренды`,
+        URL:'/Бренды',
+        img:vector,
+
+    },
+    {
+        id:5,
+        link:`Расчет стоимости`,
+        URL:'/Расчет',
+
+
+    },
+    {
+        id:6,
+        link:`Информация`,
+        URL:'/Информация',
+        img:vector,
+
+    },
+
+]
