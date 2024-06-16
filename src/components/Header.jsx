9fb42439f0c1821e5511ce5add97cf1a21bcb5ce
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-16 17:22:55 +0300

    add header input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
 // data 
-import { links } from '../data/data'
-import { Link } from 'react-router-dom'
+import { links } from '../data/data';
+import { Link } from 'react-router-dom';
 // img
-import logo from '../img/logo.svg'
-import search from '../img/search.svg'
-import reyting from '../img/reyting.svg'
-import shop from '../img/shopsvg'
+import logo from '../img/logo.svg';
+import searchIcon from '../img/search.svg'; // search nomi bilan conflict bo'lmasligi uchun o'zgartirdim
+import reyting from '../img/reyting.svg';
+import shop from '../img/shop.svg';
 
 const Header = () => {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
+  const onSearch = () => {
+    setShowSearchInput(!showSearchInput);
+  };
+
   return (
-    <header className='py-8 bg-black '>
-      <div className="containerb flex justify-between ">
-      {/* logo */}
-      <Link to='/'>
-        <img src={logo} alt="logo" />
-      </Link>
+    <header className='py-10 bg-black'>
+      <div className="containerb flex items-center justify-between">
+        {/* logo */}
+        <Link to='/'>
+          <img src={logo} alt="logo" />
+        </Link>
         {/* center */}
-        <ul className='flex space-x-12 items-center'>
-         {
-          links.map((e)=>{
-            return(
-              <li className='text-white text-sm font-semibold flex ' key={e.id}>
-                <Link to={e.URL}>{e.link}</Link>
-                {/* <img src={e.img} alt={e.link} /> */}
-              </li> 
+        <div className='flex space-x-12 items-center'>
+          {
+            showSearchInput ? (
+              <input type="serach" className=" border-2 p-4  pl-7 pr-[624px] rounded-full text-white bg-transparent" placeholder="Поиск по каталогу товаров..." />
+            ) : (
+              <ul className='flex space-x-12 items-center'>
+                {
+                  links.map((e) => {
+                    return (
+                      <li className='text-white text-sm font-semibold flex' key={e.id}>
+                        <Link to={e.URL}>{e.link}</Link>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
             )
-          })
-         }
-        </ul>
+          }
+        </div>
         {/* right */}
-        <div>
-          <Link>
-            <img src={search} alt="search" />
+        <div className='flex items-center space-x-8'>
+          <Link onClick={onSearch}>
+            <img src={searchIcon} alt="search" />
           </Link>
           <Link>
             <img src={reyting} alt="reyting" />
@@ -43,7 +56,7 @@ const Header = () => {
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
