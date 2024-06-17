7ee4c5daa5494d097c17b478de33de86f12c3b80
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-17 16:40:15 +0300

    add link responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import logo from '../img/logo.svg';
 import searchIcon from '../img/search.svg'; // search nomi bilan conflict bo'lmasligi uchun o'zgartirdim
 import reyting from '../img/reyting.svg';
 import shop from '../img/shop.svg';
+import linkmenyu from '../img/linkmenyu.svg'
 
 const Header = () => {
   const [showSearchInput, setShowSearchInput] = useState(false);
@@ -18,21 +19,29 @@ const Header = () => {
   return (
     <header className='py-10 bg-black'>
       <div className="containerb flex items-center justify-between">
-        {/* logo */}
-        <Link to='/'>
-          <img src={logo} alt="logo" />
-        </Link>
+        
+        <div className='flex'>
+          {/* logo */}
+          <Link className='mr-14' to='/'>
+            <img src={logo} alt="logo" />
+          </Link>
+
+          <button className='inline-block md:hidden'>
+            <img src={linkmenyu} alt="menyu" />
+          </button>
+        </div>
+        
         {/* center */}
         <div className='flex space-x-12 items-center'>
           {
             showSearchInput ? (
               <input type="serach" className=" border-2 p-4  pl-7 pr-[624px] rounded-full text-white bg-transparent" placeholder="Поиск по каталогу товаров..." />
             ) : (
-              <ul className='flex space-x-12 items-center'>
+              <ul className='flex   md:space-x-5 lg:space-x-8 xl:space-x-12 items-center'>
                 {
                   links.map((e) => {
                     return (
-                      <li className='text-white text-sm font-semibold flex' key={e.id}>
+                      <li className='text-white text-sm hidden md:block  lg:font-semibold flex' key={e.id}>
                         <Link to={e.URL}>{e.link}</Link>
                       </li>
                     );
@@ -43,7 +52,7 @@ const Header = () => {
           }
         </div>
         {/* right */}
-        <div className='flex items-center space-x-8'>
+        <div className='flex items-center space-x-4 lg:space-x-8'>
           <Link onClick={onSearch}>
             <img src={searchIcon} alt="search" />
           </Link>
