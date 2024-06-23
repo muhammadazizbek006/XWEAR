b9b48be007763f2244068a99ec113e79b12efb07
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-23 15:22:01 +0300

    data krasofkalar map qilib chiqarildi

--- src/components/home/Обувь.jsx
+++ src/components/home/Обувь.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// img
+import next from '../../img/next.svg'
+import reyting from '../../img/reyting.svg'
+// data
+import { krasofkalar } from '../../data/data'
 const Обувь = () => {
   return (
     <>
-        <section>
+        <section className='pt-12'>
             <div className="containerb">
-                
+                {/* top */}
+                <div className='flex justify-between'>
+                    <h2 className='text-3xl font-bold'>Обувь</h2>
+                    <Link className='flex items-center'>
+                        <p className='mr-2 text-base'>больше товаров</p>
+                        <img src={next} alt="next" />
+                    </Link>
+                </div>
+
+                <ul className='grid grid-cols-4 gap-x-8'>
+                    {
+                        krasofkalar.map((e)=>{
+                            return(
+                                <li className='bg-white'> 
+                                    <Link >
+                                        <img src={reyting} alt={e.brend} />
+                                        <img src={e.im} alt={e.title} />
+                                        <div>
+                                            <p>{e.title}</p>
+                                            <p> OT {e.narxi} ₽ </p>
+                                        </div>
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </section>
     </>
--- src/data/data.js
+++ src/data/data.js
@@ -66,3 +66,174 @@ export const herocarusel = [
         img: kepka,
     },
 ];
+
+
+
+
+// oyoq kiyimlar
+import o1 from '../img/Обувь/o1.jpg'
+import o2 from '../img/Обувь/o2.jpg'
+import o3 from '../img/Обувь/o3.jpg'
+import o4 from '../img/Обувь/o4.jpg'
+import o5 from '../img/Обувь/o5.jpg'
+import o6 from '../img/Обувь/o6.jpg'
+import o7 from '../img/Обувь/o7.jpg'
+import o8 from '../img/Обувь/o8.jpg'
+import o9 from '../img/Обувь/o9.jpg'
+import o10 from '../img/Обувь/o10.jpg'
+import o11 from '../img/Обувь/o11.jpg'
+import o12 from '../img/Обувь/o12.jpg'
+import o13 from '../img/Обувь/o13.jpg'
+import o14 from '../img/Обувь/o14.jpg'
+import o15 from '../img/Обувь/o15.jpg'
+import o16 from '../img/Обувь/o16.jpg'
+import o17 from '../img/Обувь/o17.jpg'
+import o18 from '../img/Обувь/o18.jpg'
+
+
+export const  krasofkalar = [
+    {
+        id:1,
+        im:o1,
+        title:`Nike Court Zoom Cage 2`,
+        brend:`Nike`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:2,
+        im:o2,
+        title:`Air Force 1 Ultra`,
+        brend:`Nike`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:3,
+        im:o3,
+        title:`Air Force 1 Ultra Flyknit`,
+        brend:`Air`,
+        narxi: 4299 ,
+
+    },
+    {
+        id:4,
+        im:o4,
+        title:`Nike Court Zoom 2`,
+        brend:`Nike`,
+        narxi: 4709 ,
+
+    },
+    {
+        id:5,
+        im:o5,
+        title:`Air Force 2 Ultra`,
+        brend:`Nike`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:6,
+        im:o6,
+        title:`Nike Court Zoom Cage 5`,
+        brend:`gucci`,
+        narxi: 2699 ,
+
+    },
+    {
+        id:7,
+        im:o7,
+        title:`polo Court Zoom Cage 2`,
+        brend:`polo`,
+        narxi: 7699 ,
+
+    },
+    {
+        id:8,
+        im:o8,
+        title:`Nike Court Zoom Cage 2`,
+        brend:`Nike`,
+        narxi: 4399 ,
+
+    },
+    {
+        id:9,
+        im:o9,
+        title:`Nike Court Zoom Cage 2`,
+        brend:`Nike`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:10,
+        im:o10,
+        title:`Nike Court Zoom Cage 2`,
+        brend:`Nike`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:11,
+        im:o11,
+        title:`polo Court Zoom Cage 2`,
+        brend:`polo`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:12,
+        im:o12,
+        title:`polo Court Zoom Cage 2`,
+        brend:`polo`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:13,
+        im:o13,
+        title:`polo Court Zoom Cage 2`,
+        brend:`polo`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:14,
+        im:o14,
+        title:`polo Court Zoom Cage 2`,
+        brend:`polo`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:15,
+        im:o15,
+        title:`Humara Court Zoom Cage 2`,
+        brend:`Humara`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:16,
+        im:o16,
+        title:`Humara Court Zoom Cage 2`,
+        brend:`Humara`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:17,
+        im:o17,
+        title:`Humara Court Zoom Cage 2`,
+        brend:`Humara`,
+        narxi: 4699 ,
+
+    },
+    {
+        id:18,
+        im:o18,
+        title:`Humara Court Zoom Cage 2`,
+        brend:`Humara`,
+        narxi: 4699 ,
+
+    },
+]
