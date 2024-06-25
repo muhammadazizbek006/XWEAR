6e6ebd2e8a141b5acca0a1212dc739dd0fe81dc7
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-25 15:52:43 +0300

    add basic responsive to raschitat

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// img
+import xwear from '../img/footer/xwear.svg'
+import telegram from '../img/footer/telegram.svg'
+import whatsapp from '../img/footer/whatsapp.svg'
+import week from '../img/footer/week.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className='bg-black pt-11 pb-12'>
+        <div className="containerb flex justify-between ">
+          {/* katalog */}
+          <div className='text-white'>
+            <div className=' flex flex-col space-y-2 mb-14'>
+               <h3 className='text-base font-black  '>Каталог</h3>
+               <Link className='text-base'>Одежда</Link>
+               <Link  className='text-base'>Обувь</Link>
+               <Link  className='text-base'>Аксессуары</Link>
+               <Link  className='text-base' >Расчет стоимости</Link>
+            </div>
+            <img src={xwear} alt="" />
+          </div>
+
+          {/* Информация */}
+           <div className='text-white'>
+            <div className=' flex flex-col space-y-2 mb-14'>
+               <h3 className='text-base font-black  '>Информация</h3>
+               <Link className='text-base'>Блог</Link>
+               <Link  className='text-base'>Контакты</Link>
+               <Link  className='text-base'>Доставка</Link>
+               <Link  className='text-base' >Оплата</Link>
+               <Link  className='text-base' >FAQ</Link>
+
+            </div>
+
+          </div>
+
+        {/* Контакты */}
+          <div className='text-white'>
+            <div className=' flex flex-col space-y-2 mb-14'>
+               <h3 className='text-base font-black  '>Контакты</h3>
+              <a href="email">info@xwear.info</a>
+              <p>+7 993 608 38 85</p>
+              {/* Мессенджеры */}
+              <div >
+                <h4 className='mb-2'>Мессенджеры</h4>
+                <div className='flex space-x-4'>
+                <a href="">
+                  <img src={telegram} alt="telegram" />
+                </a>
+
+                <a href="">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                </div>
+  
+              </div>
+              {/* Наши соц.сети */}
+              <div>
+                <h4 className='mb-2'>Наши соц.сети</h4>
+                <img src={week} alt="week" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/home/Raschitat.jsx
+++ src/components/home/Raschitat.jsx
@@ -8,23 +8,23 @@ import nextwhite from '../../img/nextwhite.svg'
 
 const Raschitat = () => {
   return (
-    <>
+    <>o
         <section className='raschitat pt-20 pb-72'>
             <div className="containerb flex relative ">
                 {/* left */}
-                <div className='max-w-[748px]'>
-                    <h2 className='font-black text-5xl text-white mb-5'>Рассчитать стоимость</h2>
-                    <p className='font-bold text-xl mb-6 text-white'>Если вам не удалось найти то, что искали, вы всегда можете воспользоваться автоматическим расчетом стоимость заказа на маркетплейсе Poizon, включая комиссию сервиса и доставку.</p>
+                <div className=' md:max-w-[648px] xl:max-w-[748px]'>
+                    <h2 className='font-black md:text-4xl xl:text-5xl text-white mb-5'>Рассчитать стоимость</h2>
+                    <p className='font-bold md:text-base xl:text-xl mb-6 text-white'>Если вам не удалось найти то, что искали, вы всегда можете воспользоваться автоматическим расчетом стоимость заказа на маркетплейсе Poizon, включая комиссию сервиса и доставку.</p>
                     <div className='flex  items-center space-x-14  mb-10 '>
                         {/* left */}
                         <div className='flex' >
-                            <img className='border-2 border-white py-7 px-8 rounded-full mr-5'  src={bir} alt="bir" />
+                            <img className='border-2 border-white  md:py-2   xl:py-7 md:px-5  xl:px-8 rounded-full mr-5'  src={bir} alt="bir" />
                             <p className='text-white text-base font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
                         </div>
 
                             {/* right */}
                             <div className='flex' >
-                            <img className='border-2 border-white py-7 px-8 rounded-full mr-5'  src={ikki} alt="bir" />
+                            <img className='border-2 border-white md:py-2   xl:py-7 md:px-5  xl:px-8 rounded-full mr-5'  src={ikki} alt="bir" />
                             <p className='text-white text-base font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
                         </div>
                     </div>
@@ -34,7 +34,7 @@ const Raschitat = () => {
                     </Link>
                 </div>
                 {/* right */}
-                <img className='absolute -top-56 right-0' src={telefon} alt="telefon" />
+                <img className='absolute -top-56 right-0 lg:w-[570px] xl:w-[670px]' src={telefon} alt="telefon" />
             </div>
         </section>
     </>
