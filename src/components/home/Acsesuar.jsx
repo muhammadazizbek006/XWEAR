e9c3530a7722793cd5938bc54c373274c37a45bd
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-25 21:06:32 +0300

    home page responsive

--- src/components/home/Acsesuar.jsx
+++ src/components/home/Acsesuar.jsx
@@ -15,19 +15,19 @@ const Acsesuar = () => {
          <section className='pt-12 mb-20  '>
             <div className="containerb ">
                 {/* top */}
-                <div className='flex justify-between mb-10'>
+                <div className='flex flex-col sm:flex-row items-center justify-between mb-10'>
                     <h2 className='text-3xl font-bold'>Аксессуары</h2>
                     <Link className='flex items-center'>
                         <p className='mr-2 text-base'>больше товаров</p>
                         <img src={next} alt="next" />
                     </Link>
                 </div>
-
-                <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 '>
+                <div className='flex flex-col items-center sm:items-stretch'>
+                <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-8 '>
                     {
                         slised.map((e)=>{
                             return(
-                                <li key={e.id} className=' bg-white px-3  w-80 sm:w-full'> 
+                                <li key={e.id} className=' bg-white px-3 rounded-md  w-80 sm:w-full'> 
                                     <Link className='' >
                                     <div className=' flex flex-col items-end mb-3 '>
                                         <img className='mr-5  pt-5' src={reyting} alt={e.brend} />
@@ -44,6 +44,8 @@ const Acsesuar = () => {
                         })
                     }
                 </ul>
+                </div>
+
             </div>
         </section>
     </>
--- src/components/home/Kiyimlar.jsx
+++ src/components/home/Kiyimlar.jsx
@@ -14,19 +14,19 @@ const Kiyimlar = () => {
         <section className='pt-12  '>
             <div className="containerb ">
                 {/* top */}
-                <div className='flex justify-between mb-10'>
+                <div className='flex flex-col sm:flex-row items-center justify-between mb-10'>
                     <h2 className='text-3xl font-bold'>Одежда</h2>
                     <Link className='flex items-center'>
                         <p className='mr-2 text-base'>больше товаров</p>
                         <img src={next} alt="next" />
                     </Link>
                 </div>
-
-                <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 '>
+                <div className='flex flex-col items-center sm:items-stretch'>
+                <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-8 '>
                     {
                         slised.map((e)=>{
                             return(
-                                <li key={e.id} className=' bg-white pl-3 w-80 sm:w-full'> 
+                                <li key={e.id} className=' bg-white pl-3 w-80 rounded-md sm:w-full'> 
                                     <Link className='' >
                                     <div className=' flex flex-col items-end mb-3 '>
                                         <img className='mr-5  pt-5' src={reyting} alt={e.brend} />
@@ -43,6 +43,8 @@ const Kiyimlar = () => {
                         })
                     }
                 </ul>
+                </div>
+
             </div>
         </section>
     </>
