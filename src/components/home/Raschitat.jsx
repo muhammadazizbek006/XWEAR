ac38085cb25cadf38f14f9efe25d41de6e2cdad1
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-25 18:57:52 +0300

    raschitata responsive

--- src/components/home/Raschitat.jsx
+++ src/components/home/Raschitat.jsx
@@ -8,24 +8,24 @@ import nextwhite from '../../img/nextwhite.svg'
 
 const Raschitat = () => {
   return (
-    <>o
+    <>
         <section className='raschitat pt-20 pb-72'>
-            <div className="containerb flex relative ">
+            <div className="containerb flex flex-col lg:flex-row items-center relative ">
                 {/* left */}
-                <div className=' md:max-w-[648px] xl:max-w-[748px]'>
-                    <h2 className='font-black md:text-4xl xl:text-5xl text-white mb-5'>Рассчитать стоимость</h2>
+                <div className=' max-w-[608px] xl:max-w-[748px]'>
+                    <h2 className=' font-medium sm:font-semibold md:font-black text-4xl xl:text-5xl text-white mb-5'>Рассчитать стоимость</h2>
                     <p className='font-bold md:text-base xl:text-xl mb-6 text-white'>Если вам не удалось найти то, что искали, вы всегда можете воспользоваться автоматическим расчетом стоимость заказа на маркетплейсе Poizon, включая комиссию сервиса и доставку.</p>
-                    <div className='flex  items-center space-x-14  mb-10 '>
+                    <div className='flex flex-col sm:flex-row   items-center sm:items-start lg:space-x-14  mb-10 '>
                         {/* left */}
-                        <div className='flex' >
-                            <img className='border-2 border-white  md:py-2   xl:py-7 md:px-5  xl:px-8 rounded-full mr-5'  src={bir} alt="bir" />
-                            <p className='text-white text-base font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
+                        <div className='flex items-center mb-5' >
+                            <img className='border-2 border-white py-5    px-6   xl:px-8  rounded-full mr-5'  src={bir} alt="bir" />
+                            <p className='text-white text-sm  xl:text-base font-medium xl:font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
                         </div>
 
                             {/* right */}
-                            <div className='flex' >
-                            <img className='border-2 border-white md:py-2   xl:py-7 md:px-5  xl:px-8 rounded-full mr-5'  src={ikki} alt="bir" />
-                            <p className='text-white text-base font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
+                        <div className='flex items-center' >
+                            <img className='border-2 border-white  py-5    px-5  xl:px-8 rounded-full mr-5'  src={ikki} alt="bir" />
+                            <p className='text-white text-sm  xl:text-base font-medium xl:font-semibold'>Подробная, пошаговая статья о том, как установить приложение Poizon</p>
                         </div>
                     </div>
                     <Link className='flex items-center bg-black w-64 py-6 pl-6 rounded-md'>
@@ -34,7 +34,7 @@ const Raschitat = () => {
                     </Link>
                 </div>
                 {/* right */}
-                <img className='absolute -top-56 right-0 lg:w-[570px] xl:w-[670px]' src={telefon} alt="telefon" />
+                <img className='absolute -top-56 right-0 hidden lg:block lg:w-[500px] xl:w-[650px]' src={telefon} alt="telefon" />
             </div>
         </section>
     </>
