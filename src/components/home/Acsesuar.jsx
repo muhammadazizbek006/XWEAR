66d9e79a4ac9306ecd8c1d6bc55c5e9b175df7df
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-25 12:06:24 +0300

    add

--- src/components/home/Acsesuar.jsx
+++ src/components/home/Acsesuar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// img
+import next from '../../img/next.svg'
+import reyting from '../../img/reytingb.svg'
+// data
+import { kiyimlar } from '../../data/data'
 const Acsesuar = () => {
+    const slised = kiyimlar.slice(0, 4)
+
   return (
-    <div>Acsesuar</div>
+    <>
+         <section className='pt-12  '>
+            <div className="containerb ">
+                {/* top */}
+                <div className='flex justify-between mb-10'>
+                    <h2 className='text-3xl font-bold'>Одежда</h2>
+                    <Link className='flex items-center'>
+                        <p className='mr-2 text-base'>больше товаров</p>
+                        <img src={next} alt="next" />
+                    </Link>
+                </div>
+
+                <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 '>
+                    {
+                        slised.map((e)=>{
+                            return(
+                                <li key={e.id} className=' bg-white pl-3 w-80 sm:w-full'> 
+                                    <Link className='' >
+                                    <div className=' flex flex-col items-end mb-3 '>
+                                        <img className='mr-5  pt-5' src={reyting} alt={e.brend} />
+                                        <img src={e.img} alt={e.title} />
+
+                                    </div>
+                                        <div>
+                                            <p className='text-xl'>{e.title}</p>
+                                            <p> от  {e.narxi} ₽ </p>
+                                        </div>
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </section>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 // import Hero from '../components/home/Hero'
 import OyoqKiyimlar from '../components/home/OyoqKiyimlar'
 import Kiyimlar from '../components/home/Kiyimlar'
+import Acsesuar from '../components/home/Acsesuar'
 const Home = () => {
   return (
     <>
       {/* <Hero/> */}
       <OyoqKiyimlar/>
       <Kiyimlar/>
+      <Acsesuar/>
     </>
   )
 }
