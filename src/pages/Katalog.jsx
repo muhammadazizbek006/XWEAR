323022933112ed7604dc457b7c463ba016278d1a
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-26 08:12:33 +0300

    new page

--- src/pages/Katalog.jsx
+++ src/pages/Katalog.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Katalog = () => {
   return (
-    <div>Katalog</div>
+    <>
+        <section>
+            <div className="containerb">
+                
+            </div>
+        </section>
+    </>
   )
 }
 
