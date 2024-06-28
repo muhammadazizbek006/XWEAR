78816da3bd549aea0139c7ad10c60ef653676dd4
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-28 05:54:15 +0300

    add katalog detail null file

---
+++ src/pages/KatalogDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const KatalogDetail = () => {
+  return (
+    <>
+        <section>
+            <div className="containerb">
+                
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default KatalogDetail
