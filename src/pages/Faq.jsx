82e7ba0828a90ca0a7b52aa90c8f5e8e8467e06b
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-06-29 13:29:32 +0300

    faq add

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import { Faqs } from '../data/data';
+ 
+// img
+import plus from '../img/faqs/plus.svg';
+import minus from '../img/faqs/minus.svg';
 
 const Faq = () => {
+  const [open, setOpen] = React.useState(null);
+  
+  const handleOpen = (value) => {
+    setOpen(open === value ? null : value);
+  };
+
   return (
     <>
-        <section>
-            <div className="containerb">
-                <h2 className='text-3xl font-black mb-7'>FAQ</h2>
-            </div>
-        </section>
-    </>
-  )
-}
+      <section className='pt-8 pb-28'>
+        <div className="containerb">
+          <h2 className='text-3xl font-black mb-7'>FAQ</h2>
 
-export default Faq
+          {Faqs.map((faq, e) => (
+            <Accordion key={faq.id} open={open === e}>
+              <div className='border-b-2'>
+                <AccordionHeader className='flex justify-between p items-center py-5  ' onClick={() => handleOpen(e)}>
+                  <p className='text-base font-semibold'>{faq.savol}</p>
+                  <img className='right-0' src={open === e ? minus : plus} alt={faq.savol} />
+                </AccordionHeader>
+                {open === e && (
+                  <AccordionBody className='flex justify-between pb-5'>
+                    <p className='text-base '>{faq.javob}</p>
+                  </AccordionBody>
+                )}
+              </div>
+            </Accordion>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Faq;
