8e78bbbe44a679ffc09435bca3a45f5028fc8bc9
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-05 15:24:57 +0300

    add yoqtrilganlar page

---
+++ src/pages/Yoqtrilganlar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Yoqtrilganlar = () => {
+  return (
+    <>
+        <section>
+            <div className="containerb">
+                
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Yoqtrilganlar
