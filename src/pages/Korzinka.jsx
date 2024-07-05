a4c5216b5d2f7396e2f96334133778baf2183be9
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-05 08:51:24 +0300

    korzinka page to'grilandi

--- src/pages/Korzinka.jsx
+++ src/pages/Korzinka.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 // img
 import delet from '../img/korzinka/delete.svg'
+import maxsulotyoq from '../img/korzinka/maxsulotyoq.png'
 import { deleteUserOfferLinkData } from "../store/slice/productsWishlistDataSlice";
 
 const Korzinka = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const tanlanganMahsulotlar = useSelector(
     (store) => store.tanlanganMahsulotlar.data
   );
@@ -17,39 +19,40 @@ const Korzinka = () => {
     if (count > 0) setCount(count - 1);
   };
   const handleDelete = (id) => {
-    dispatch(deleteUserOfferLinkData(id))
-
+    dispatch(deleteUserOfferLinkData(id));
   };
+
+
   return (
     <>
       <section className="py-10 ">
-        <div className="containerb">
-          <h2 className="text-3xl font-black mb-4">Рассчитать стоимость</h2>
-          {/* malumot */}
-          <div className="text-base font-medium max-w-[1013px] mb-10 ">
-            <p className="mb-9">
-              Мы ежедневно добавляем новые товары на сайт, но если вам не
-              удалось найти то, что искали, вы всегда можете воспользоваться
-              автоматическим расчетом стоимость заказа на маркетплейсе Poizon,
-              включая комиссию сервиса и доставку. Если у вас еще нет приложения
-              Poizon, тогда можете посмотреть статью ниже. В приложении есть
-              любые кроссовки и одежда дешевле на 40% чем в РФ.
-            </p>
-            <p>
-              Если вы не хотите устанавливать приложение, вы всегда можете
-              написать нам в Telegram или WhatsApp, какую вещь хотите купить.
-              Для этого напишите ее название или пришлите фотографию к нам в
-              чат. Наши операторы сами найдут нужный вам товар и рассчитают его
-              стоимость.
-            </p>
-          </div>
-          {/* tanlangan maxsulot map */}
-          <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-12">
-            {tanlanganMahsulotlar &&
-              tanlanganMahsulotlar.map((e) => (
-                <li className="shadow-lg relative flex flex-col items-center text-center pb-6 rounded-md">
+        {tanlanganMahsulotlar.length > 0 ? (
+          <div className="containerb">
+            <h2 className="text-3xl font-black mb-4">Рассчитать стоимость</h2>
+            {/* malumot */}
+            <div className="text-base font-medium max-w-[1013px] mb-10 ">
+              <p className="mb-9">
+                Мы ежедневно добавляем новые товары на сайт, но если вам не
+                удалось найти то, что искали, вы всегда можете воспользоваться
+                автоматическим расчетом стоимость заказа на маркетплейсе Poizon,
+                включая комиссию сервиса и доставку. Если у вас еще нет приложения
+                Poizon, тогда можете посмотреть статью ниже. В приложении есть
+                любые кроссовки и одежда дешевле на 40% чем в РФ.
+              </p>
+              <p>
+                Если вы не хотите устанавливать приложение, вы всегда можете
+                написать нам в Telegram или WhatsApp, какую вещь хотите купить.
+                Для этого напишите ее название или пришлите фотографию к нам в
+                чат. Наши операторы сами найдут нужный вам товар и рассчитают его
+                стоимость.
+              </p>
+            </div>
+            {/* tanlangan maxsulot map */}
+            <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-12">
+              {tanlanganMahsulotlar.map((e) => (
+                <li key={e.id} className="shadow-lg relative flex flex-col items-center text-center pb-6 rounded-md">
                   {/* delete */}
-                  <button className="absolute  top-5 right-5" onClick={() => handleDelete(e.id)}>
+                  <button className="absolute top-5 right-5" onClick={() => handleDelete(e.id)}>
                     <img className=" " src={delet} alt="delet" />  
                   </button>
                   <img className="" src={e.img} alt={e.title} />
@@ -57,30 +60,39 @@ const Korzinka = () => {
                     <p>{e.brend}</p>
                     <p className="mb-6">{e.title}</p>
                    
-                    <div className="flex items-center space-x-4  ">
-                     <button
-                      onClick={decrement}
-                      className="px-6 text-3xl hover:bg-red-500 hover:text-white duration-300  bg-gray-200 text-gray-700 rounded"
-                    >
-                      - 
-                    </button>
-                    <span className="mx-2">{count}</span>
-                    <button
-                      onClick={increment}
-                      className="px-6 text-2xl hover:bg-green-500 hover:text-white duration-300  bg-gray-200 text-gray-700 rounded"
-                    >
-                      +
-                    </button>
-                  </div>
+                    <div className="flex items-center space-x-4">
+                      <button
+                        onClick={decrement}
+                        className="px-6 text-3xl hover:bg-red-500 hover:text-white duration-300 bg-gray-200 text-gray-700 rounded"
+                      >
+                        - 
+                      </button>
+                      <span className="mx-2">{count}</span>
+                      <button
+                        onClick={increment}
+                        className="px-6 text-2xl hover:bg-green-500 hover:text-white duration-300 bg-gray-200 text-gray-700 rounded"
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
                 </li>
               ))}
-          </ul>
-        </div>
+            </ul>
+          </div>
+        ) : (
+          <div className="containerb flex flex-col items-center ">
+            <div className=" max-w-[637px] flex flex-col items-center text-center ">
+              <img className="w-20 h-24 object-cover mb-5" src={maxsulotyoq} alt="" />
+              <h3 className="text-2xl font-extrabold mb-4">Ваша корзина на данный момент пуста.</h3>
+              <p className="text-base mb-5">Прежде чем приступить к оформлению заказа, вы должны добавить некоторые товары в корзину. На странице "Каталог" вы найдете много интересных товаров.</p>
+              <Link to='/Katalog' className='px-7 py-6 bg-black rounded-md text-white text-sm'>Перейти в каталог</Link>
+            </div>
+          </div>
+        )}
       </section>
     </>
   );
 };
 
 export default Korzinka;
-
