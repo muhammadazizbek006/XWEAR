e8dbca16b546e579b62bdf79f1d2b6c892d349c2
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-13 09:49:48 +0300

    a

--- src/pages/SotibOlish.jsx
+++ src/pages/SotibOlish.jsx
@@ -5,15 +5,15 @@ const SotibOlish = () => {
     <>
       <section className="bg-[#f4f7fd]">
         <div className="containerb">
-          <h2 className="text-4xl font-semibold">Оформление заказа</h2>
+          <h2 className="text-4xl font-semibold mb-6">Оформление заказа</h2>
           <form className="" action="">
             <ul>
               {/* left */}
-              <li className="bg-white shadow-md p-12">
+              <li className="bg-white rounded-lg shadow-md p-12 w-[1014px]" >
                     {/* top */}
                     <div>
                     {/* telefotn F.I.O */}
-                    <div className="flex">
+                    <div className="flex justify-between">
                         {/* telefon */}
                         <div className="flex flex-col">
                         <label
@@ -21,13 +21,13 @@ const SotibOlish = () => {
                             htmlFor=""
                         >
                             Телефон *
-                        </label>
-                        <input type="tel" value={+998} />
+                        </label >
+                        <input className="w-[452px] py-2 px-3 rounded-md border-2 border-[#c2c6d1] " type="tel" placeholder="+998" />
                         </div>
                         {/* F.I.O */}
-                        <div>
-                        <label htmlFor=""> Ф.И.О *</label>
-                        <input type="tel" value={+998} />
+                        <div className="flex flex-col">
+                        <label  className="text-sm font-medium text-labelcolor mb-4" htmlFor=""> Ф.И.О *</label>
+                        <input className="w-[452px] py-2 px-3 rounded-md border-2 border-[#c2c6d1] " type="tel" value={+998} />
                         </div>
                     </div>
                     </div>
