7a1c967aa66d724d6f780639ba0cbe5e33188a94
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-16 14:22:01 +0300

    !

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import Header from '../components/Header'
-import { Outlet } from 'react-router-dom'
+
+import { useEffect  } from "react";
+import Header from "../components/Header";
+import { Outlet, useLocation,  } from "react-router-dom";
 import Footer from '../components/Footer'
 
 const MainLayout = () => {
+  const location = useLocation();
+  const home = location.pathname === "/";
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+  const path = location.pathname;
   return (
     <div className='flex flex-col min-h-screen'>
         <Header/>
