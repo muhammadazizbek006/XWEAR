2b81e600007deecfbae8295f33dac0cf5143242d
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-17 22:11:30 +0300

    a

--- src/components/home/OyoqKiyimlar.jsx
+++ src/components/home/OyoqKiyimlar.jsx
@@ -139,42 +139,8 @@ const dispatch = useDispatch()
         })
     }
     <div className="swiper-pagination" style={{ marginTop: '30px' }}></div> {/* Pagination uchun joy */}
-</Swiper>
+        </Swiper>
 
-            {/* <ul className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-y-6 gap-x-8">
-              {KatalogMahsulotlari.map((e) => {
-                return (
-                  <li key={e.id} className="  relative px-4 py-3  w-80 ">
-                     <button
-                      className="absolute right-4"
-                      onClick={() => toggleLike(e)}
-                    >
-                      <img
-                        src={likedProducts[e.id] ? like3 : like2}
-                        alt={e.brend}
-                      />
-                    </button>
-
-                    <Link to={`/product/${e.id}`} className="">
-                      <div className="  mb-3 ">
-                        <img src={e.img} alt={e.title} />
-                      </div>
-                      <div className="mb-1 flex flex-col justify-between">
-                        <p className="text-xl font-semibold ">{e.title}</p>
-                         <p className="text-base"><span className="text-xl font-semibold">категория: </span>{e.kategoria}</p>
-                      </div>
-                    </Link> */}
-                    {/* shop btn va kategoria */}
-                    {/* <div className="flex justify-between">
-                        <p  className="text-base"> <span className="text-base font-semibold">расходы: </span>от {e.narxi} ₽ </p>
-                      <button onClick={() => mahsulotniWishlistgaQoshish(e)} className="bg-black px-8 py-2 rounded-md ">
-                        <img src={shop} alt="" />
-                      </button>
-                    </div> */}
-                  {/* </li>
-                );
-              })}
-            </ul> */}
           </div>
         </div>
       </section>
