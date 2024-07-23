845c0ee8363e8b4ef032c1e5f166b3201344b14b
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-07-23 16:06:40 +0300

    add enter

--- src/components/home/Acsesuar.jsx
+++ src/components/home/Acsesuar.jsx
@@ -60,6 +60,7 @@ const Acsesuar = () => {
         <div className="flex flex-col sm:flex-row items-center justify-between mb-10">
           <h2 className="text-3xl font-bold">Аксессуары</h2>
           <Link to="/katalog/acsesuar" className="flex items-center">
+          
             <p className="mr-2 text-base">больше товаров</p>
             <img src={next} alt="next" />
           </Link>
